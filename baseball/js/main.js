0e078c0d09cc95bb8830386bf25a8d641134c521
Robert <amd3439@gmail.com>
2016-03-19 15:19:32 -0700

    Revert "adding files via upload"

--- baseball/js/main.js
+++
@@ -1,4 +0,0 @@
-$(window).load(function(){
-    $('#bar1').animate({marginLeft: "100px", width: "300px"}, 1800, function(){});
-    $('#bar2').animate({marginLeft: "1200px", width: "300px"}, 1800, function(){});
-});
